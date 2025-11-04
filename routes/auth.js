f535495a86d41a4732e61aa1cfc65ab25898bdfd
SufiyaaanShaikh <sufisk2526@gmail.com>
2025-11-04 22:31:48 +0530

    Cookies issue

--- routes/auth.js
+++ routes/auth.js
@@ -50,9 +50,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
-      domain: "employee-login-system-frontend.vercel.app", // ✅ frontend domain
-      path: "/",
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
